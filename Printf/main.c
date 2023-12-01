d6251437c25ce93f7bea0568b8b40f162846baf1
Guylian Finet <gfinet@fu-r4-p5.s19.be>
2023-12-01 17:25:15 +0100

    printf with good makefile

--- Printf/main.c
+++
@@ -1,9 +0,0 @@
-#include "ft_printf.h"
-#include <stdio.h>
-
-int main(void)
-{
-	char *s = NULL;
-	printf("%d --> ", printf("%c %d %i %x %X %p %s %%\n", 2000, 3, 0, 255, 255, NULL, NULL));
-	printf("%d --> ", ft_printf("%c %d %i %x %X %p %s %%\n", 2000, 3, 0, 255, 255, NULL, NULL));
-}
