55971934fd822fc40e0aaaa262a42c9981fb722a
Guylian Finet <gfinet@mi-r4-p2.s19.be>
2024-02-26 19:21:15 +0100

    have fun

--- Fractol/main_fractol.c
+++ Fractol/main_fractol.c
@@ -6,7 +6,7 @@
 /*   By: gfinet <gfinet@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 16:15:11 by gfinet            #+#    #+#             */
-/*   Updated: 2024/02/26 18:40:48 by gfinet           ###   ########.fr       */
+/*   Updated: 2024/02/26 19:18:24 by gfinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,16 @@ int	main(int argc, char **argv)
 	int i;
 	int start_x;
 	int start_y;
+	double x;
+	double y;
+	double ox;
+	double oy;
 	
 	i = 0;
-	start_x = 2000/2;
+	start_x = 2000/2; // [0,0] = [1000, 625], [-1, 0] = [-500, 625], [0, 1] = [1000, 1125]
+	// x = start_x + mandelbrot_x * 500
+	// y = start_y + mandelbrot_y * 500
+
 	start_y = 1250/2;
 	if (argc > 1)
 	{
@@ -49,15 +56,33 @@ int	main(int argc, char **argv)
 		img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
 		my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
 		mlx_put_image_to_window(mlx, win_ptr, img.img, 0, 0);
-		while (i < 20)
+		while (i < 11)
 		{
-			printf("%i (%f,", i, mandelbrot_x(i, -0.3));
-			printf("%f)\n", mandelbrot_y(i, -0.3));
-			my_mlx_pixel_put(&img, mandelbrot_x(i, 0)*100, start_y + mandelbrot_y(i, 0)*100, 0x00FF0000);
-			mlx_put_image_to_window(mlx, win_ptr, img.img, 0, start_y);
+			if (i == 0)
+			{
+				x = 0;
+				y = 0;
+			}
+			else
+			{
+				x = ox * ox - oy - oy + 0.3;
+				y = 2 * ox * oy + 0.3;
+				ox = x;
+				oy = y;
+			}
+			printf("%i (%f,", i, x);
+			printf("%f)\n", y);
+			// printf("%i ",i);
+			my_mlx_pixel_put(&img, start_x + x * 500, start_y - y * 500, 0x00FF0000);
+			mlx_put_image_to_window(mlx, win_ptr, img.img, 0, 0);
+			// if (i < 100)
+			// 	my_mlx_pixel_put(&img, 5 + i, start_y, 0x00FF0000);
+			// else
+			// 	my_mlx_pixel_put(&img, 5 + 100, start_y - 100 + i, 0x00FF0000);
+			// mlx_put_image_to_window(mlx, win_ptr, img.img, 0, 0);
 			i++;
 		}
-
+		//mlx_put_image_to_window(mlx, win_ptr, img.img, 0, 0);
 		mlx_loop(mlx);
 	}
 	return (0);
--- Fractol/mandelbrot.c
+++ Fractol/mandelbrot.c
@@ -6,18 +6,23 @@
 /*   By: gfinet <gfinet@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 16:34:51 by gfinet            #+#    #+#             */
-/*   Updated: 2024/02/26 18:25:04 by gfinet           ###   ########.fr       */
+/*   Updated: 2024/02/26 19:07:05 by gfinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-double	mandelbrot(int x, double c)
-{
-	if (x <= 0)
-		return (0);
-	return (exp2(mandelbrot(x - 1, c)) + c);
-}
+// double	mandelbrot(double x, double y, double c)
+// {
+// 	double r;
+// 	double i;
+
+// 	r = x * x - y * y;
+// 	i = 2.0 * x * y;
+// 	if (x <= 0)
+// 		return (0);
+// 	return (mandelbrot(x - 1, c) *  + c); // Vec(x, y) + c
+// }
 
 double mandelbrot_x(int n, double a)
 {
