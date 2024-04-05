573cae6edfa94f2bcb92f0b89d1de9261783e503
Guylian Finet <gfinet@fu-r4-p5.s19.be>
2024-04-05 19:01:08 +0200

    correction update

--- Pipex/correction.c
+++ Pipex/correction.c
@@ -6,7 +6,7 @@
 /*   By: gfinet <gfinet@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 18:26:01 by Gfinet            #+#    #+#             */
-/*   Updated: 2024/04/02 17:11:04 by gfinet           ###   ########.fr       */
+/*   Updated: 2024/04/05 18:46:46 by gfinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ static pid_t launch_pipex(char **arg, char **envp)
 	proc = fork();
 	if (proc == 0)
 	{
-		ft_printf("-----./pipex infile %s ", arg[2]); 
+		ft_printf("-----./pipex %s %s ",arg[1], arg[2]); 
 		ft_printf("%s > ", arg[3]);
 		ft_printf("%s-----\n", arg[4]);
 		execve(arg[0], arg, envp);
@@ -47,7 +47,7 @@ int main(int argc, char **argv, char **envp)
 	char	**arg;
 	char	**cmds;// *cmds[] = {"cat", "ls",  "wc", "echo", NULL};
 
-	nb_cmd = argc - 1;
+	nb_cmd = argc - 2;
 	nb_proc = nb_cmd * nb_cmd + 1;
 
 	cmds = malloc(sizeof(char *) * (nb_cmd + 3));
@@ -56,27 +56,32 @@ int main(int argc, char **argv, char **envp)
 	cmds[nb_cmd + 2] = NULL;
 	i = 1;
 	mkdir("./test", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-	while (i - 1< nb_cmd)
+	open("./test/no_perm_in_c", O_CREAT, 0000);
+	open("./test/no_perm_out_c", O_CREAT, 0000);
+	open("./test/no_perm_out2_c", O_CREAT, 0000);
+	open("./test/all_perm_in_c", O_CREAT, 0777);
+	open("./test/all_perm_out_c", O_CREAT, 0777);
+	while (i - 1 < nb_cmd)
 	{
-		cmds[i] = ft_strdup(argv[i]);
+		cmds[i] = ft_strdup(argv[i + 1]);
 		i ++;
 	}
-	proc = malloc(sizeof(pid_t) * nb_proc);
-	status = malloc(sizeof(int) * nb_proc);
+	proc = malloc(sizeof(pid_t) * (nb_proc + 3));
+	status = malloc(sizeof(int) * (nb_proc + 3));
 
 	arg = malloc(sizeof(char *) * 5);
 	arg[0] = ft_strdup("./pipex");
-	arg[1] = ft_strdup("infile");
+	arg[1] = ft_strdup(argv[1]);
 
 	i = 1;
-	while (i - 1< nb_cmd && cmds[i])
+	while (i - 1 < nb_cmd && cmds[i])
 	{
 		j = 1;
 		arg[2] = ft_strdup(cmds[i]);
 		while (j - 1 < nb_cmd && cmds[j])
 		{
 			arg[3] = ft_strdup(cmds[j]);
-			arg[4] = ft_strdup("test/c");
+			arg[4] = ft_strdup("./test/c");
 			arg[4] = ft_stradd(arg[4], ft_itoa(nb_cmd * (i - 1) + (j - 1)));
 			proc[nb_cmd*(i-1)+(j-1)] = launch_pipex(arg, envp);
 			j++;
@@ -86,19 +91,38 @@ int main(int argc, char **argv, char **envp)
 		free(arg[2]);
 		i++;
 	}
+	free(arg[1]);
+	arg[2] = ft_strdup("cat -e");
+	arg[3] = ft_strdup("wc -w");
+
+	arg[1] = ft_strdup("./test/no_perm_in_c");
+	arg[4] = ft_strdup("./test/no_perm_out_c");
+	proc[nb_cmd*(i-1)+(j-1) + 1] = launch_pipex(arg, envp);//=> no_in + no_out1 = no_out1
+	free(arg[4]);
+	arg[4] = ft_strdup("./test/all_perm_out_c");
+	proc[nb_cmd*(i-1)+(j-1) + 2] = launch_pipex(arg, envp);//=> no_in + y_out1 = y_out1
+	free(arg[1]);
+	free(arg[4]);
+	arg[1] = ft_strdup("./test/all_perm_in_c");
+	arg[4] = ft_strdup("./test/no_perm_out2_c");
+	proc[nb_cmd*(i-1)+(j-1) + 3] = launch_pipex(arg, envp);//=> y_in + no_out2 = no_out2
 	i = 0;
-	
-	while (i < nb_proc)
+	while (i < nb_proc + 3)
 	{
 		waitpid(proc[i], &status[i], 0);
 		i++;
 	}
+	i = 0;
+
 	proc[nb_proc - 1] = fork();
 	if (proc[nb_proc - 1] == 0)
 		execve("correction.sh", cmds, 0);
 		//execve("correction.sh",(char*[]){"correction.sh","cat ls wc echo", NULL}, 0);
 	free(arg[0]);
 	free(arg[1]);
+	free(arg[2]);
+	free(arg[3]);
+	free(arg[4]);
 	free(arg);
 	free(proc);
 	i = 0;
@@ -109,5 +133,6 @@ int main(int argc, char **argv, char **envp)
 	}
 	free(cmds);
 	free(status);
+	//system("leaks : correction");
 	return (0);
 }
--- Pipex/correction.sh
+++ Pipex/correction.sh
@@ -11,7 +11,9 @@ flag=0
 #echo "${arr[${#arr[@]}]- 1 }"
 
 args=("$@")
-#echo ${args[0]} ${args[1]} ${args[2]}
+# echo ${args[0]} 
+# echo ${args[1]} 
+# echo $#
 
 for (( i=0; i + 1<$#; i++ ))
 do
@@ -22,7 +24,7 @@ do
 	done
 done
 
-echo "Testing..."
+echo "Testing classic command"
 for (( i=0; i + 1<$#; i++ ))
 do
 	for (( j=0; j + 1<$#; j++ ))
@@ -41,6 +43,45 @@ do
 	done
 done
 
+echo "Testing permissions command"
+echo "Yolo" > test/no_perm_in_b > /dev/null 2>&1
+echo "" > test/no_perm_out_b > /dev/null 2>&1
+chmod 000 test/no_perm_in_b > /dev/null 2>&1
+chmod 000 test/no_perm_out_b > /dev/null 2>&1
+< test/no_perm_in_b cat -e | wc -w > test/no_perm_out_b > /dev/null 2>&1
+chmod 777 test/no_perm_out_b > /dev/null 2>&1
+chmod 777 test/no_perm_out_c > /dev/null 2>&1
+res=$(diff test/no_perm_out_b test/no_perm_out_c)
+if [ $? -ne 0 ]
+then
+	echo "Error \"< test/no_perm_out_b cat -e | wc -w > test/no_perm_out_b test/no_perm_out_c"
+	flag=1
+fi
+
+echo "" > test/all_perm_out_b > /dev/null 2>&1
+chmod 777 test/all_perm_out_b > /dev/null 2>&1
+< test/no_perm_in_b cat -e | wc -w > test/all_perm_out_b > /dev/null 2>&1
+res=$(diff test/all_perm_out_b test/all_perm_out_c)
+if [ $? -ne 0 ]
+then
+	echo "Error \"< test/no_perm_in_b cat -e | wc -w > test/all_perm_out_b all_perm_out_c"
+	flag=1
+fi
+
+echo "Yolo" > test/all_perm_in_b > /dev/null 2>&1
+touch ./test/no_perm_out2_b > /dev/null 2>&1
+chmod 000 test/no_perm_out2_b > /dev/null 2>&1
+< test/all_perm_in_b cat -e | wc -w > test/no_perm_out2_b > /dev/null 2>&1
+chmod 777 test/no_perm_out2_c > /dev/null 2>&1
+chmod 777 test/no_perm_out2_b > /dev/null 2>&1
+res=$(diff test/no_perm_out2_b test/no_perm_out2_c)
+if [ $? -ne 0 ]
+then
+	echo "Error \"< test/all_perm_in_b cat -e | wc -w > test/no_perm_out2_b test/no_perm_out2_c"
+	flag=1
+fi
+
+
 if [ $flag -eq 0 ]
 then
 	echo "Everything is good"
@@ -58,5 +99,22 @@ do
 		rm test/b$((${@: -1}*i+j))
 	done
 done
+
+chmod 777 test/all_perm_out_b
+chmod 777 test/no_perm_in_b
+chmod 777 test/no_perm_in_c
+chmod 777 test/no_perm_out_b
+chmod 777 test/no_perm_out2_b
+
+rm ./test/no_perm_in_b
+rm ./test/no_perm_out_b
+rm ./test/no_perm_out2_b
+rm ./test/all_perm_in_b
+rm ./test/all_perm_out_b
+rm ./test/no_perm_in_c
+rm ./test/no_perm_out_c
+rm ./test/no_perm_out2_c
+rm ./test/all_perm_in_c
+rm ./test/all_perm_out_c
 rmdir test
 echo "cleaning done"
