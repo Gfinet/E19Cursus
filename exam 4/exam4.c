291600d2d7f7224188fde99fc7088528565498fa
Guylian Finet <gfinet@mi-r3-p3.s19.be>
2024-10-15 21:14:27 +0200

    finished

--- exam 4/exam4.c
+++ exam 4/exam4.c
@@ -6,7 +6,7 @@
 /*   By: gfinet <gfinet@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 14:49:45 by gfinet            #+#    #+#             */
-/*   Updated: 2024/10/15 19:52:06 by gfinet           ###   ########.fr       */
+/*   Updated: 2024/10/15 21:14:00 by gfinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,7 +64,7 @@ int count_proc(char **av, char **sep)
 
 void launch_cmd(t_cmd *cmd, char **envp, int pip[2], int pipe_fd[2])
 {
-	printf("launch cmd %s\n", cmd->buff[0]);
+	//printf("launch cmd %s\n", cmd->buff[0]);
     // Si c'est une commande avec un pipe en sortie
     if (pip[0] && cmd->sep[cmd->cur] == '|')
         if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
@@ -81,7 +81,24 @@ void launch_cmd(t_cmd *cmd, char **envp, int pip[2], int pipe_fd[2])
 		close(pipe_fd[1]);
 
     execve(cmd->buff[0], cmd->buff, envp);
+
     exit(EXIT_FAILURE);
+}
+
+int cd(t_cmd *cmd)
+{
+	int k = 0;
+
+	while (cmd->buff[k])
+		k++;
+	if (k > 2)
+		return (write(2, "error: cd: bad arguments\n", 25), 1);
+	k = 0;
+	while (cmd->buff[1][k])
+		k++;
+	if (chdir(cmd->buff[1]) == -1)
+		return (write(2, "error: cd: cannot change directory to ", 38), write(2, cmd->buff[1], k), write(2, "\n", 1), 1);
+	return (0);	
 }
 
 
@@ -105,7 +122,7 @@ int main(int ac, char **av, char **env)
         return (1);
 
     cmd.nb_cmd = count_proc(av, &cmd.sep);
-    printf("nb : %d\nsep : [%s]\n", cmd.nb_cmd, cmd.sep);
+    // printf("nb : %d\nsep : [%s]\n", cmd.nb_cmd, cmd.sep);
     cmd.proc = malloc(sizeof(pid_t) * (cmd.nb_cmd + 1));
 
     while (i++ < ac)
@@ -113,29 +130,32 @@ int main(int ac, char **av, char **env)
         cmd.buff = cut_cmd(&i, av);
         if (!cmd.buff)
             return (write(2, "error: fatal\n", 13), 1);
-
-        //print_cmd(cmd.buff);
-
-        pip[0] = (cmd.nb_cmd > 1 && cmd.sep[cmd.cur] == '|');
-        pip[1] = (cmd.cur && cmd.cur < cmd.nb_cmd && cmd.sep[cmd.cur - 1] == '|');
-
-        if (pip[0] && printf("PIPE\n") && pipe(pipe_fd) < 0)
-            return (write(2, "error: fatal\n", 13), 1);
-
-        cmd.proc[cmd.cur] = fork();
-        if (cmd.proc[cmd.cur] == 0)
-			launch_cmd(&cmd, env, pip, (int[2]){inf, pipe_fd[1]});
-
-        if (inf != -1)
-            close(inf);
-        if (pip[0])
-		{
-            close(pipe_fd[1]);
-            inf = pipe_fd[0];
-        }
+			 //print_cmd(cmd.buff);
+		
+		if (!strcmp(cmd.buff[0], "cd"))
+			return (free(cmd.proc), cd(&cmd));
 		else
-			inf = -1;
+        {
+			pip[0] = (cmd.nb_cmd > 1 && cmd.sep[cmd.cur] == '|');
+			pip[1] = (cmd.cur && cmd.cur < cmd.nb_cmd && cmd.sep[cmd.cur - 1] == '|');
 
+			if (pip[0] && pipe(pipe_fd) < 0)
+				return (write(2, "error: fatal\n", 13), 1);
+
+			cmd.proc[cmd.cur] = fork();
+			if (cmd.proc[cmd.cur] == 0)
+				launch_cmd(&cmd, env, pip, (int[2]){inf, pipe_fd[1]});
+
+			if (inf != -1)
+				close(inf);
+			if (pip[0])
+			{
+				close(pipe_fd[1]);
+				inf = pipe_fd[0];
+			}
+			else
+				inf = -1;
+		}
         free(cmd.buff);
         cmd.cur++;
     }
@@ -173,7 +193,8 @@ main()
 		else
 			save_fd = -1;
 	}
-	wait_proc();
+	if (wait_proc(proc, status) == -1)
+		write(proc bug);
 	free_all();
 }
 
