058f4397d48d47c0c242abc79d23ec3b8d151680
Guylian Finet <gfinet@mac.home>
2025-02-06 17:36:39 +0100

    up

--- CPP/CPP06/ex00/ScalarConverter.cpp
+++ CPP/CPP06/ex00/ScalarConverter.cpp
@@ -6,15 +6,15 @@
 /*   By: Gfinet <gfinet@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 15:09:43 by Gfinet            #+#    #+#             */
-/*   Updated: 2025/02/04 18:53:04 by Gfinet           ###   ########.fr       */
+/*   Updated: 2025/02/06 17:13:34 by Gfinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
-
 /*
 ** ------------------------------- CONSTRUCTOR --------------------------------
 */
+
 
 ScalarConverter::ScalarConverter()
 {
@@ -39,7 +39,7 @@ ScalarConverter::~ScalarConverter()
 ** --------------------------------- OVERLOAD ---------------------------------
 */
 
-ScalarConverter &				ScalarConverter::operator=( ScalarConverter const & rhs )
+ScalarConverter &ScalarConverter::operator=( ScalarConverter const & rhs )
 {
 	(void)rhs;
 	return *this;
@@ -50,6 +50,45 @@ ScalarConverter &				ScalarConverter::operator=( ScalarConverter const & rhs )
 ** --------------------------------- METHODS ----------------------------------
 */
 
+// static bool isAll(int func(int), std::string in)
+// {
+// 	for (size_t i=0; i < in.length(); i++)
+// 	{
+// 		if (!func(in.c_str()[0]))
+// 			return false;
+// 	}
+// 	return true;
+// }
+
+static void addUp(char c, long double *val)
+{
+	*val += 10;
+	*val += c - '0';
+}
+
+static long double getVal(std::string in)
+{
+	long double res = 0, vir = 0;
+	int sign, pow=1;
+	bool dot = false;
+
+	sign = - (in[0] == '-');
+	for (size_t i = (sign == -1); i < in.length(); i++)
+	{
+		if (in[i] == '.')
+			dot = true;
+		if (!dot && isdigit(in[i]))
+			addUp(in[i], &res);
+		else if (dot && isdigit(in[i]))
+		{
+			pow *= 10;
+			addUp(in[i], &vir);
+		}
+	}
+	vir *= pow;
+	res += vir;
+	return res;
+}
 
 static void printChar(char c)
 {
@@ -59,26 +98,79 @@ static void printChar(char c)
 	std::cout << "double : " << (double)c << std::endl;
 }
 
+static bool isChar(std::string in)
+{
+	return (in.length() == 1) && isalpha(in[0]);
+}
+
+static bool isInt(std::string in)
+{
+	bool sign;
+
+	sign = (in[0] == '+' || in[0] == '-');
+	for (size_t i = sign; i<in.length(); i++)
+	{
+		if(!isdigit(in[i]))
+			return false;
+	}
+	return true;
+}
+static bool isFloat(std::string in)
+{
+	bool sign, dot = false, f = false;
+
+	sign = (in[0] == '+' || in[0] == '-');
+	for (size_t i = sign; i < in.length(); i++)
+	{
+		if ((!isdigit(in[i]) && in[i] != '.' && in[i] != 'f') ||
+			(dot && in[i] == '.') || (f && in[i]))
+			return false;
+		if (in[i] == '.')
+			dot = true;
+		if(in[i] == 'f')
+			f = true;
+	}
+	return true;
+}
+static bool isDouble(std::string in)
+{
+	bool sign, dot = false;
+
+	sign = (in[0] == '+' || in[0] == '-');
+	for (size_t i = sign; i < in.length(); i++)
+	{
+		if ((!isdigit(in[i]) && in[i] != '.')|| (dot && in[i] == '.'))
+			return false;
+		if (in[i] == '.')
+			dot = true;
+	}
+	return true;
+}
+static bool isSpecial(std::string in)
+{
+	return (in == "nan" || in == "nanf" || in == "+inf" || in == "-inf" || in == "+inff" || in == "-inff");
+}
+
 static e_type getType(std::string in)
 {
 	size_t dot = in.find(".");
-	size_t f = in.find("f");
+	// size_t f = in.find("f");
 
-	if (in.length() == 1 && isascii(in[0]))
+	std::cout << "in : " << in << ", len : " << in.length() << std::endl;
+	if (isChar(in))
 		return CHAR;
-	else if (in == "nan" || in == "nanf")
+	else if (isSpecial(in))
+		return SPECIAL;
+	else if (dot != std::string::npos)
 	{
-
+		if (isDouble(in))
+			return DOUBLE;
+		else if (isFloat(in))
+			return FLOAT;
 	}
-	else if (!(dot == std::string::npos))
-	{
-		// int
-	}
-	else if (!(f == std::string::npos))
-	{
-		//double
-	}
-	else if()
+	else if (isInt(in))
+		return INT;
+	return INVALID;
 
 }
 
@@ -92,11 +184,38 @@ void ScalarConverter::convert(std::string a)
 	double *d;
 
 	a_void = &a;
-	if (a.length() == 1 && isascii((char)a[0]))
-		return printChar((char)a[0]);
+	// if (a.length() == 1 && isascii((char)a[0]))
+	// 	return printChar((char)a[0]);
+	std::cout << getType(a) << std::endl;
+	switch (getType(a))
+	{
+	case 0: //SPECIAL
+		//convertSpecial(a);
+		break;
+
+	case 1: //char
+		printChar(a[0]);
+		break;
+
+	case 2: //int
+		//convertInt(a);
+		break;
+
+	case 3: //float
+		//convertFloat(a);
+		break;
+
+	case 4: //double
+		//convertDouble(a);
+		break;
+
+	default: // invalid
+		break;
+	}
+	if (0)
+		;
 	else
 	{
-		get_type();
 		c = (char*)(a_void);
 		i = (int*)(a_void);
 		f = (float*)(a_void);
--- CPP/CPP06/ex00/main.cpp
+++ CPP/CPP06/ex00/main.cpp
@@ -6,22 +6,33 @@
 /*   By: Gfinet <gfinet@student.s19.be>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 15:46:35 by Gfinet            #+#    #+#             */
-/*   Updated: 2025/02/04 18:38:52 by Gfinet           ###   ########.fr       */
+/*   Updated: 2025/02/06 17:36:22 by Gfinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 #include "limits.h"
 
+#define FLOAT_MIN 1.175494351e-38
+#define FLOAT_MAX 3.402823466e+38
 int main()
 {
 	std::string intmax(std::to_string(INT_MAX));
+	std::string floatmin(std::to_string(FLOAT_MIN));
+	std::string floatmax(std::to_string(FLOAT_MAX));
+
 
 	ScalarConverter::convert("a");
+	std::cout << std::endl;
+	ScalarConverter::convert("aa");
 	std::cout << std::endl;
 	ScalarConverter::convert("0");
 	std::cout << std::endl;
 	ScalarConverter::convert("5");
+	std::cout << std::endl;
+	ScalarConverter::convert("+5");
+	std::cout << std::endl;
+	ScalarConverter::convert("-5a");
 	std::cout << std::endl;
 	ScalarConverter::convert(intmax);
 	std::cout << std::endl;
@@ -33,8 +44,6 @@ int main()
 	std::cout << std::endl;
 	ScalarConverter::convert("nanf");
 	std::cout << std::endl;
-
-	std::cout << std::string:: ("5.5");
 
 	return 0;
 }
