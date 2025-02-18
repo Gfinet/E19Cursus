26545687286a3c99cb14d3571babe41a7ffb0315
Guylian Finet <gfinet@mi-r3-p5.s19.be>
2025-02-18 15:59:22 +0100

    CPP07 end

--- CPP/CPP07/ex02/Array.hpp
+++ CPP/CPP07/ex02/Array.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Array.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Gfinet <gfinet@student.s19.be>             +#+  +:+       +#+        */
+/*   By: gfinet <gfinet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 20:46:41 by Gfinet            #+#    #+#             */
-/*   Updated: 2025/02/15 18:16:41 by Gfinet           ###   ########.fr       */
+/*   Updated: 2025/02/18 15:57:26 by gfinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ class Array
 {
 private:
 	T *_lst;
-	unsigned int _len;
+	unsigned int _size;
 public:
 	Array();
 	Array(const Array<T> &x);
@@ -34,37 +34,54 @@ public:
 	unsigned int size() const;
 };
 
+template <typename T> static T* memcpy(T* orig, size_t len)
+{
+	T* cpy;
+	
+	if (!orig || !len)
+		return 0;
+	cpy = new T[len];
+	for (size_t i = 0; i < len; i++)
+		cpy[i] = orig[i];
+	std::cout << "end\n" ;
+	return cpy;
+}
+
 template <class T> Array<T>::Array()
 {
-	this->_lst = new T[0];
+	std::cout << "Empty constructor called\n";
+	this->_lst = 0;
 	std::cout << this->_lst << std::endl;
-	_len = 0;
+	_size = 0;
 }
 
 template <class T> 
 Array<T>::Array(unsigned int x)
 {
+	std::cout << "constructor with " << x << " elem called\n";
 	this->_lst = new T[x];
-	_len = x;
+	_size = x;
 }
 
 template <class T> 
-Array<T>::Array(const Array<T> &x)
+Array<T>::Array(const Array<T> &x) : _lst(new T[x._size]), _size(x._size)
 {
-	*this = x;
+	// std::cout << "copy constructor called\n";
+	for (size_t i=0; i < x._size; i++)
+		_lst[i] = x._lst[i];
 }
 
 template <typename T> 
 Array<T> Array<T>::operator=(const Array<T> &x)
 {
+	std::cout << "Assignement operator called\n";
 	if (this != &x)
 	{
-		if (_len != 0)
-			delete[] _lst;
-		_lst = new T[x._len];
-		for (size_t i=0; i < x._len; i++)
-			_lst[i] = x[i];
-		_len = x._len;
+		delete[] _lst;
+		_lst = new T[x._size];
+		for (size_t i=0; i < x._size; i++)
+			_lst[i] = x._lst[i];
+		_size = x._size;
 	}
 	return *this;
 }
@@ -74,7 +91,7 @@ T& Array<T>::operator[](const unsigned int x) const
 {
 	try
 	{
-		if (x > _len)
+		if (x >= _size)
 			throw std::out_of_range("Exception : Out of range");
 		return this->_lst[x];
 	}
@@ -93,7 +110,7 @@ Array<T>::~Array()
 
 template <class T> unsigned int Array<T>::size() const
 {
-	return _len;
+	return _size;
 }
 
 #endif
--- CPP/CPP07/ex02/main.cpp
+++ CPP/CPP07/ex02/main.cpp
@@ -3,27 +3,81 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Gfinet <gfinet@student.s19.be>             +#+  +:+       +#+        */
+/*   By: gfinet <gfinet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 17:22:51 by Gfinet            #+#    #+#             */
-/*   Updated: 2025/02/15 18:09:49 by Gfinet           ###   ########.fr       */
+/*   Updated: 2025/02/18 15:58:57 by gfinet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+#define MAX_VAL 750
 #include "Array.hpp"
+#include <cstdlib>
 
 int main()
 {
-	Array<int> a(5), d(5), c(a);
-	Array<int> b;
+	{
+		Array<int> a(5);
+		Array<int> c(a);
+		Array<int> b;
+		Array<int> d(5);
 
-	a[1] = 3;
-	std::cout << "a[1] : " << a[1] << std::endl;
-	std::cout << "c[1] : " << c[1] << std::endl;
-	std::cout << "a[99] : " << a[99] << std::endl;
-	std::cout << "b[1] : " << b[1] << std::endl;
-	d = a;
-	std::cout << "b[1] : " << b[1] << std::endl;
+		a[1] = 3;
+		std::cout << "a[1] : " << a[1] << std::endl;
+		std::cout << "c[1] : " << c[1] << std::endl;
+		std::cout << "a[99] : " << a[99] << std::endl;
+		d = a;
+		std::cout << "d = a" << std::endl;
+		std::cout << "d[1] : " << d[1] << std::endl;
+	}
+	{
+		Array<int> numbers(MAX_VAL);
+		int* mirror = new int[MAX_VAL];
+		std::srand(time(NULL));
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			const int value = std::rand();
+			numbers[i] = value;
+			mirror[i] = value;
+		}
+		//SCOPE
+		{
+			Array<int> tmp = numbers;
+			Array<int> test(tmp);
+		}
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			if (mirror[i] != numbers[i])
+			{
+				std::cerr << "didn't save the same value!!" << std::endl;
+				return 1;
+			}
+		}
+		try
+		{
+			numbers[-2] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		try
+		{
+			numbers[MAX_VAL] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
 
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			numbers[i] = rand();
+		}
+		delete[] mirror;//
+		
+	}
 	return 0;
 }
